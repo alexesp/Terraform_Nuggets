02b9c8d4d804c392a31b81800494f109ce57e8f8
alexesp <alexizaes@hotmail.com>
2024-12-12 00:26:34 +0100

    enlazar subredes

---
+++ subnets.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "subnet-south2a" {
+  vpc_id = aws_vpc.vpc-south.id
+  cidr_block = "10.5.10.0/24"
+  availability_zone = "eu-south-2a"
+  tags = {
+    Name="subnet-south2a"
+  }
+}
+resource "aws_subnet" "subnet-south2b" {
+  vpc_id = aws_vpc.vpc-south.id
+  cidr_block = "10.5.20.0/24"
+  availability_zone = "eu-south-2b"
+  tags = {
+    Name="subnet-south2b"
+  }
+}
+resource "aws_subnet" "subnet-south2c" {
+  vpc_id = aws_vpc.vpc-south.id
+  cidr_block = "10.5.30.0/24"
+  availability_zone = "eu-south-2a"
+  tags = {
+    Name="subnet-south2c"
+  }
+}
