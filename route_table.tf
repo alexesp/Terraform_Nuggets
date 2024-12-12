aa5192ed05f0f7ff302f44bbd9ae380e436b9506
alexesp <alexizaes@hotmail.com>
2024-12-12 01:36:24 +0100

    enlazar la tabela a subred

--- route_table.tf
+++ route_table.tf
@@ -4,10 +4,21 @@ vpc_id = aws_vpc.vpc-south.id
 
 route{
     cidr_block = "0.0.0.0/0"
-    gateway_id=aws_internet_gateway.gw-south
+    gateway_id=aws_internet_gateway.gw-south.id
 }
 
 tags = {
   Name="routes-south"
 }
+}
+
+resource "aws_route_table_association" "route_assoc_south2a"{
+ subnet_id = aws_subnet.subnet-south2a.id
+ route_table_id = aws_route_table.routes-south.id
+
+}
+resource "aws_route_table_association" "route_assoc_south2b"{
+ subnet_id = aws_subnet.subnet-south2b.id
+ route_table_id = aws_route_table.routes-south.id
+ 
 }
