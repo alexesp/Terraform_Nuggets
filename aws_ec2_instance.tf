203314ee14816e208622da6480805726eb72a506
alexesp <alexizaes@hotmail.com>
2024-12-13 11:41:39 +0100

    añadir grupo de securidad

--- aws_ec2_instance.tf
+++ aws_ec2_instance.tf
@@ -2,8 +2,14 @@ resource "aws_instance" "server01" {
   ami = "ami-01d67cc599f23990b"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.subnet-south2a.id
-
+  key_name = "tf_par_clave"
+  associate_public_ip_address = true
+  
   tags = {
     Name = "Instance_ubuntu_24_04"
   }
 }
+
+#  output "public_ip" {
+#    value       = aws_instance.server01.public_ip
+#  }
--- aws_vpc.tf
+++ aws_vpc.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "vpc-south" {
   cidr_block = "10.5.0.0/16"
 
   tags = {
-    Name="tf-vpc-south"
+    Name="tf-vpc-south2"
   }
 }
 
