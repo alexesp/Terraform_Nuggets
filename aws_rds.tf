c5f61eb1e4f1fc921b32aae308a89910904a8445
alexesp <alexizaes@hotmail.com>
2024-12-13 13:29:57 +0100

    crear db MySql

--- aws_rds.tf
+++ aws_rds.tf
@@ -1,7 +1,19 @@
 resource "aws_db_subnet_group" "subnet_group_db_south2" {
-    provider = aws.aws-eu-south-2
-    name = "terraform"
+    #provider = aws.aws-eu-south-2
+    name = "terraform2"
     subnet_ids = [aws_subnet.subnet-south2a.id, aws_subnet.subnet-south2b.id,aws_subnet.subnet-south2c.id]
 
   
 }
+resource "aws_db_instance" "rdsdb-suth2" {
+    allocated_storage = 10
+    db_name = "terraform_db"
+    engine = "mysql"
+    engine_version = "8.0.39"
+    instance_class = "db.t4g.micro"
+    username = "admin_123"
+    password = "aDMIn_321"
+    skip_final_snapshot = true
+    db_subnet_group_name = aws_db_subnet_group.subnet_group_db_south2.name
+}
+ 
--- aws_security_group.tf
+++ aws_security_group.tf
@@ -1,25 +1,25 @@
-resource "aws_security_group" "http" {
-  name        = "admin"
-  description = "Creacion de grupo de seguridad para HTTP/SSH"
+# resource "aws_security_group" "http" {
+#   name        = "admin"
+#   description = "Creacion de grupo de seguridad para HTTP/SSH"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # -1 indica que se permite cualquier protocolo (TCP, UDP, ICMP, etc.).
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1" # -1 indica que se permite cualquier protocolo (TCP, UDP, ICMP, etc.).
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
